a00481bf432f72e3b6b7fb00dde08138ef2a7c94
Britta <britta.haupt@hotmail.de>
2025-06-30 22:32:38 +0200

    final final version

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "<html>";
+echo "  <head>";
+echo "    <meta charset=\"UTF-8\">";
+echo "    <title>C(h)at Bot</title>";
+echo "    <link rel=\"stylesheet\" href=\"styles.css\">";
+echo "  </head>";
+echo "  <body>";
+      include 'chatbot.php';
+echo "  </body>";
+echo "</html>";
+?>
