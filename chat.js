e188dba83c4a2bb12fa3e864a694d7d3f5261d5f
Britta Haupt <britta.haupt@dermalog.com>
2025-06-30 09:13:11 +0000

    added simple leave timer, cats don't stay forever

--- chat.js
+++ chat.js
@@ -1,29 +1,48 @@
+window.onload = function () {
+  leaveTimer(); // it's a cat. It won't stay for long
+}
+
 function sendMessage() {
-    let userInput = document.getElementById('user_input');
-    let messageText = userInput.value.trim();
+  let userInput = document.getElementById('user_input');
+  let messageText = userInput.value.trim();
+  if (messageText === '') {
+    return;
+  }
+  let chatBox = document.getElementById('chat');
+  let userMessage = document.createElement('div');
+  userMessage.textContent = messageText;
+  userMessage.classList.add('chat', 'sent');
+  chatBox.appendChild(userMessage);
+  // reply from the chatbot
+  // TODO cat reply
+  setTimeout(() => {
+    let chatbotResponse = document.createElement('div');
+    chatbotResponse.textContent = 'Hey there! How can i help you !';
+    chatbotResponse.classList.add('chat-message', 'received');
+    chatBox.appendChild(chatbotResponse);
+    // Scroll to bottom of chat box
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }, 1000);
+  userInput.value = ''; // Clear user input after sending message
+}
 
-    if (messageText === '') {
-        return;
-    }
-
+// TODO: maybe randomize this a bit?
+function leaveTimer( ) {
+  // const timer = 10000; // 5 minutes in milliseconds
+  const timer = 300000; // 5 minutes in milliseconds
+  setTimeout(() => {
     let chatBox = document.getElementById('chat');
-    let userMessage = document.createElement('div');
-    userMessage.textContent = messageText;
-    userMessage.classList.add('chat', 'sent');
-
-    chatBox.appendChild(userMessage);
-
-    // Simulate response from chatbot after a delay
+    let leaveMessage = document.createElement('div');
+    leaveMessage.textContent = 'Your cat left the room.';
+    leaveMessage.classList.add('chat', 'received');
+    chatBox.appendChild(leaveMessage);
+    chatBox.scrollTop = chatBox.scrollHeight; // Scroll to bottom of chat box
     setTimeout(() => {
-        let chatbotResponse = document.createElement('div');
-        chatbotResponse.textContent = 'Hey there! How can i help you !';
-        chatbotResponse.classList.add('chat-message', 'received');
+          history.back(); // Go back to the previous page
+    }, 2000); // delay before going back to show leave message
+  }, timer);
+}
 
-        chatBox.appendChild(chatbotResponse);
+function catReply() {
 
-        // Scroll to bottom of chat box
-        chatBox.scrollTop = chatBox.scrollHeight;
-    }, 1000);
-
-    userInput.value = ''; // Clear user input after sending message
 }
